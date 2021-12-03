574d47d85348c1f00ea0873f7395f5eb6975c181
Todd Fisher <todd.fisher@gmail.com>
2021-12-03 14:34:24 -0500

    cleaning up notifications

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Tray, Menu} = require('electron')
 const path = require('path')
 const ipc = require('electron').ipcMain;
 
@@ -23,6 +23,7 @@ function createWindow () {
   })
 
   // and load the index.html of the app.
+  mainWindow.webContents.setUserAgent(mainWindow.webContents.getUserAgent() + " CTM");
   mainWindow.loadFile('index.html');
   mainWindow.webContents.once('dom-ready', () => { mainWindow.show();});
 
@@ -43,20 +44,44 @@ function createWindow () {
     console.log("Open window received");
     mainWindow.show();
   });
+
+  return mainWindow;
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  const mainWindow = createWindow();
   
   app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
-})
+
+// TODO: get tray menu items from login, dynamically update when incoming phone call and modify based on inbound/outbound call
+// TODO: this is not showing up in the packaged app...
+// TODO: https://api.calltrackingmetrics.com/api/v1/accounts/{account_id}/available_statuses?normalized=1
+  tray = new Tray('./public/packaged-icons/icons/png/16x16.png');
+  tray.setToolTip('Open Phone')
+  tray.on('click', () => {
+    console.log("clicked the tray");
+    mainWindow.show();
+  });
+  /*
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Available', type: 'radio' , checked: true },
+    { label: 'Not Available', type: 'radio' },
+    { label: 'Custom Status A', type: 'radio'},
+    { label: 'Custom Status B', type: 'radio' },
+    { label: 'Custom Status C', type: 'radio' }
+  ]);
+  tray.setContextMenu(contextMenu)
+  */
+}).catch( (e) => {
+  console.error("error in ready:", e.message);
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
--- renderer.js
+++ renderer.js
@@ -11,30 +11,13 @@ onload = () => {
 
     const event_message = e.message; 
 
-  //   if(event_message.includes("got open panel:")){
-  //     //Open Window automatically
-  //     ipcRenderer.send('open-window', 'pageTwo');
-  //     // 
-  //     new Notification("Outgoing Call Initiated", { body: JSON.stringify(e.message) })
-  //     .onclick = () => {
-  //       ipcRenderer.send('open-window', 'pageTwo');
-  //     }
-  //   }
-
-
-    if(event_message.includes("device incoming call:")){
+    if (event_message.includes("device incoming call:")) {
       //Open Window automatically
       ipcRenderer.send('open-window', 'pageTwo');
       // 
-      new Notification("Incoming Call Notification", { body: JSON.stringify(e.message) })
-      .onclick = () => {
+      new Notification("Incoming Call Notification", { body: JSON.stringify(e.message) }).onclick = () => {
         ipcRenderer.send('open-window', 'pageTwo');
       }
     }
-
-  //   new Notification("Dialer Notification", { body: JSON.stringify(e) })
-  //   .onclick = () => {
-  //     ipcRenderer.send('open-window', 'pageTwo');
-  //   }
   })
 }
